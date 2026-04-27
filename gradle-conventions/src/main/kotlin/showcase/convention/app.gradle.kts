102810486c862e393cdc18330df710dbe4e44980
mitsuharu yoshiyama <m.yoshiyama@gmail.com>
2026-04-28 07:45:52 +0900

    build: enable -Xannotation-default-target=param-property for resource ID lint

--- gradle-conventions/src/main/kotlin/showcase/convention/app.gradle.kts
+++ gradle-conventions/src/main/kotlin/showcase/convention/app.gradle.kts
@@ -45,6 +45,10 @@ androidApplication {
 
 tasks.withType<KotlinCompile>().configureEach {
     compilerOptions.jvmTarget.set(AndroidConfig.jvmTarget)
+    // KT-73255: apply annotations like @StringRes / @DrawableRes / @ApplicationContext
+    // to both the constructor parameter and the property/field, silencing Lint warnings
+    // about resource ID annotations on `val` constructor params.
+    compilerOptions.freeCompilerArgs.add("-Xannotation-default-target=param-property")
 }
 
 dependencies {
